b287e88a1b356c977cc8a6498490dd4190431aa6
ildeberto <ildeberto34@gmail.com>
2024-10-17 22:22:24 +0100

    refact: clean up and widget tests

---
+++ lib/app/consts.dart
@@ -0,0 +1,11 @@
+/// The maximum opacity value for colors (fully opaque).
+const double fullOpacityValue = 1.0;
+
+/// The threshold value for determining high luminance.
+/// A luminance value greater than this indicates a light color.
+const double highLuminanceThreshold = 0.5;
+
+/// The default duration for animations in the application.
+/// This value can be used to maintain consistency across animations.
+const Duration defaultAnimationDuration = Duration(milliseconds: 400);
+
